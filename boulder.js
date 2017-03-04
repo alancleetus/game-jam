4a0e8cc305745a1102eb8c6c4ace38335af2e852
cleetus <cleetusa@ECS.CSUS.EDU>
2017-03-04 13:34:51 -0800

    adding boulder class

---
+++ boulder.js
@@ -0,0 +1,28 @@
+class boulder
+{
+	constructor(boulderId,gameWindow)
+	{
+		this.boulder = $('#boulder');
+		this.gameWindow = gameWindow;
+		this.speed = 10;
+	}
+	
+	moveUp(){
+		this.player.css("margin-bottom",  ((this.getBottomMargin() + this.getSpeed()) + 'px'));
+	}
+	
+	getSpeed(){
+		return this.speed;
+	}
+	
+	 isOutsideTopBoundry(){
+		//boulder bottom margin > 1000
+		return this.getBottomMargin() > gameWindow.;
+	}
+	
+	
+	 getTopMargin(){
+		return parseInt(this.player.css("margin-top"));
+	}
+	
+}
