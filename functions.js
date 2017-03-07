28b0b157a71d17530cc5759d00019a90df8c76cc
alancleetus <alanclee95@mgmail.com>
2017-03-07 11:51:38 -0800

    added proper intendation for code, added comments, got rid of some old commented out code, and backgrounds now reset back to position when new round starts

--- functions.js
+++ functions.js
@@ -2,84 +2,89 @@
 //game window. dont want player to move past the screen
 //boulder-player collision
 
-$(document).ready(function(){
-
-document.onkeydown = checkKey;
-document.onkeyup = stopPlayer;
-
-var score = 0;
-var roundNum=1;
-var scoreIntervalId = setInterval(function(){
-	score++;
-	$('#val').text(""+score);
-//console.log(score);
+$(document).ready(function()
+{
+	document.onkeydown = checkKey;
+	document.onkeyup = stopPlayer;
+	
+	//player score
+	var score = 0;
+	//game round number
+	var roundNum = 1;
+	
+	//increments score every 200ms
+	var scoreIntervalId = setInterval(function()
+	{
+		score++;
+		$('#val').text(""+score);
+		//console.log(score);
 	}, 200);
 	
-$("#roundVal").text(roundNum);
+	//inserts round number into the webpage
+	$("#roundVal").text(roundNum);
 	
-var gameWindow =  new GameWindow('gameWindow');
-var mPlayer = new Player($('#player'),gameWindow);
-//var playerDiv =  document.getElementById("player");
-//var gameWindow =  document.getElementById("gameWindow");
-var keyPressed=false;
-function checkKey(e) {
+	var gameWindow =  new GameWindow('gameWindow');
+	var mPlayer = new Player($('#player'),gameWindow);
 	
-    e = e || window.event;
+	var keyPressed=false;
+	function checkKey(e) 
+	{
+		e = e || window.event;
 
-  /*  if (e.keyCode == '38') {
-        // up arrow
-		// move player down
-		//increase top margin
-    }
-    else if (e.keyCode == '40') {
-        // down arrow
-		// move player up
-    }
-    else */
-	// console.log(mPlayer.getLeftMargin());
-	if (e.keyCode == '37') {
-		if(keyPressed){
-			return;
-		}
-		keyPressed=true;
+		// console.log(mPlayer.getLeftMargin());
 
-       // left arrow
-	   // move player right
-	   //set player to right moving animation
-		mPlayer.moveRight();
+		/*left arrow clicked > move player right*/
+		if (e.keyCode == '37') 
+		{
+			if(keyPressed)	return;
+	
+			keyPressed=true;
+
+			//player class will set player to right moving animation
+			mPlayer.moveRight();
 		
-	   //validate that player isn't already out of bounds, if so, move to end of right side
-	   if(mPlayer.isOutsideRightBoundry()){
-		   mPlayer.setToRightSideOfGameWindow();
-	   }else{
-		   
-		   //move backgrounds
-		   	$('#parallaxLayer1').css("margin-left", (parseInt($('#parallaxLayer1').css("margin-left"))+3)+'px'   );
-			$('#parallaxLayer2').css("margin-left", (parseInt($('#parallaxLayer2').css("margin-left"))+1)+'px'   );
-			$('#runText').css("margin-left", (parseInt($('#runText').css("margin-left"))+2)+'px'   );
+			/* ******************************************************
+			 * validate that player isn't already out of bounds, 	*
+			 * if so, move to player to end of right side		*
+			 * else, move player right and parallax background		*
+			 ********************************************************/
+			if(mPlayer.isOutsideRightBoundry())
+			{
+		   		mPlayer.setToRightSideOfGameWindow();
+			}
+			else{
+		   		//move backgrounds
+		   		$('#parallaxLayer1').css("margin-left", (parseInt($('#parallaxLayer1').css("margin-left"))+3)+'px'   );
+				$('#parallaxLayer2').css("margin-left", (parseInt($('#parallaxLayer2').css("margin-left"))+1)+'px'   );
+				$('#runText').css("margin-left", (parseInt($('#runText').css("margin-left"))+2)+'px'   );
 	
-	   }
-	  
-    }
-    else if (e.keyCode == '39') {
-		if(keyPressed)
-			return;
-		keyPressed=true;
-       // right arrow
-	   // move player left
-	   //set player to left moving animation
-
-	   mPlayer.moveLeft();
-	    //validate that player isn't already out of bounds, if so, move to end of right side
-	   if(mPlayer.isOutsideLeftBoundry()){
-		   mPlayer.setToLeftSideOfGameWindow();
-	   }else{
-		   	$('#parallaxLayer1').css("margin-left", (parseInt($('#parallaxLayer1').css("margin-left"))-3)+'px'   );
-			$('#parallaxLayer2').css("margin-left", (parseInt($('#parallaxLayer2').css("margin-left"))-1)+'px'   );
-			$('#runText').css("margin-left", (parseInt($('#runText').css("margin-left"))-2)+'px'   );
+	   		}/*end parallaxing*/
+    		}/*end left arrow click*/
+		
+    		else if (e.keyCode == '39') /*right arrow cliked > move player left*/
+		{
+			if(keyPressed)	return;
+			
+			keyPressed=true;
+       			
+			//player class will set player to left moving animation
+	   		mPlayer.moveLeft();
+	    
+			/* ******************************************************
+			 * validate that player isn't already out of bounds, 	*
+			 * if so, move to player to end of right side		*
+			 * else, move player right and parallax background	*
+			 ********************************************************/
+	  		if(mPlayer.isOutsideLeftBoundry()){
+				mPlayer.setToLeftSideOfGameWindow();
+	   		}else{
+				//move backgrounds
+		   		$('#parallaxLayer1').css("margin-left", (parseInt($('#parallaxLayer1').css("margin-left"))-3)+'px'   );
+				$('#parallaxLayer2').css("margin-left", (parseInt($('#parallaxLayer2').css("margin-left"))-1)+'px'   );
+				$('#runText').css("margin-left", (parseInt($('#runText').css("margin-left"))-2)+'px'   );
 	
-	   }
-    }
+	  		}/*end parallaxing*/
+    		}/*end right arrow click*/
 
 	
 	// move asteroid/boulder-player
@@ -93,55 +98,68 @@ function checkKey(e) {
 	//after every 5 seconds increase freqency  and increase speed every 7 seconds of boulders
 	
 
-}
+	}
 
-function stopPlayer(){
-	keyPressed=false;
-	//change player animation to standing still
-	$('#player').css("background-image",  "url('./assets/sprites/UpsidedownStanding.gif')");
-	
-}
+	/*changes the player animation to standing still*/
+	function stopPlayer(){
 
+		keyPressed=false;
 
-function moveDeathBoulder(){
-	$("#giantBoulder").animate({top: '-1500px'},10000-(roundNum*600), function(){
-		
-userWon();
-		
-	});
-
-	
-
-	
-}
-moveDeathBoulder();
-
-function  userWon(){
-	var playerLeftMargin = mPlayer.getLeftMargin();
-	if(playerLeftMargin >= 900 || playerLeftMargin <= 100){
-		
-		var s=confirm("You WON!. Continue");
-		console.log(s);
-		if(s){
-			roundNum++;
-			$("#roundVal").text(roundNum);
-			$("#giantBoulder").animate({top: '800px'},10);
-			$('#player').css("margin-left","450px");
-
-			moveDeathBoulder();
-		}else{
-			location.reload();
-		}
-	}else{
-		var x =confirm("You LOST! Your score was : "+score+" Would you like to go again?");
-		console.log(x);
-		if(x){
-			location.reload();
-		}else{
-			clearInterval(scoreIntervalId);
-		}
-			
+		//change player animation to standing still
+		$('#player').css("background-image",  "url('./assets/sprites/UpsidedownStanding.gif')");
 	
 	}
-}
+
+	/*moves the boulder up by */
+	function moveDeathBoulder()
+	{
+		/*animate boulder and check if user won*/
+		$("#giantBoulder").animate({top: '-1500px'},10000-(roundNum*600), function(){	userWon();	});	
+	}
+
+	moveDeathBoulder();
+
+	function  userWon()
+	{
+		var playerLeftMargin = mPlayer.getLeftMargin();
+	
+	
+		if(playerLeftMargin >= 900 || playerLeftMargin <= 100)
+		{
+			/* check if player wants to play again
+			 * if yes > increase round number and reset boulder player, and stars	
+			 * else reset page 
+			 */
+			var s=confirm("You WON!. Continue");
+			console.log(s);
+			if(s)
+			{
+				roundNum++;
+				$("#roundVal").text(roundNum);
+				$("#giantBoulder").animate({top: '800px'},10);
+				$('#player').css("margin-left","450px");
+				/*reset background*/
+				$('#parallaxLayer1').css("left","-50%");
+				$('#parallaxLayer2').css("left","-50%");
+				$('#runText').css("margin-left","32%");
+				$('#runText').css("margin-top","40%");
+
+				moveDeathBoulder();
+			}
+			else
+			{
+				location.reload();
+			}
+		}else{
+			var x =confirm("You LOST! Your score was : "+score+" Would you like to go again?");
+			console.log(x);
+			if(x){
+				location.reload();
+			}else{
+				clearInterval(scoreIntervalId);
+			}
+			
+	
+		}
+	}
 });
